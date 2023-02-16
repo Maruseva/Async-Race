de6ccfc6105ed882ffdd7c3de68b121126ba046d
maruseva <maruseva@yandex.ru>
2023-02-16 18:53:33 +0300

    refactoring

--- src/components/car/car.view.ts
+++ src/components/car/car.view.ts
@@ -2,29 +2,22 @@ import "./car.style.css";
 import carImg from "../../assests/img/car.svg";
 import flagImg from "../../assests/img/flag.png";
 
-export class Car {
-    private readonly selector: string;
-    constructor(selector: string) {
-        this.selector = selector;
-    }
-
-    public render(name?: string, color?: string): void {
-        const wrap = <HTMLDivElement>document.querySelector(this.selector);
+export class CarInRow {
+    public static render(car: Car): Element {
         const div = <HTMLDivElement>document.createElement('div');
         div.innerHTML = `<div>
             <div>
                 <button>SELECT</button>
                 <button>REMOVE</button>
-                <span></span>
+                <span>${car.name}</span>
             </div>
             <div class="car__item">
                 <button>A</button>
                 <button>B</button>
-                <span></span>
                 <img src="${carImg}" alt="car" class="car__carImg">
                 <img src="${flagImg}" alt="flag" class="car__flagImg">
             </div>
         </div>`;
-        wrap.appendChild(div);
+        return div;
     }
 }
