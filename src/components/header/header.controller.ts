7b18017dd78f6e52e26f104762e1759674b0782d
maruseva <maruseva@yandex.ru>
2023-04-12 15:17:19 +0300

    delete selector

--- src/components/header/header.controller.ts
+++ src/components/header/header.controller.ts
@@ -2,13 +2,11 @@ import { Router } from "../../router/router";
 import { Header } from "./header.view";
 
 export class HeaderController { 
-    private readonly selector: string;
     private header: Header;
     private router: Router;
     constructor(selector: string) {
-        this.selector = selector;
-        this.header = new Header(this.selector);
-        this.router = new Router(this.selector);
+        this.header = new Header(selector);
+        this.router = new Router(selector);
     }
 
     public render(): void {
